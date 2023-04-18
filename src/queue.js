3a131427db487dbb556196099f8a807debe8e2af
SergeyFedoruk <7282289@tut.by>
2023-04-18 03:18:32 +0300

    feat: add remuve from list Tuesday 18.04.2023 02_22

--- src/queue.js
+++ src/queue.js
@@ -24,7 +24,8 @@ class Queue {
     getUnderlyingList() {
         //throw new NotImplementedError('Not implemented');
         // remove line with error and write your code here
-        return(this.elements[this.head]);
+
+        return constructor(this.elements);
 
     }
 
